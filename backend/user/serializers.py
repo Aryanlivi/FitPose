2ff83b698938c9af973e5718b40b67ee2a825913
AnkitShrestha-Xenserv <aankitshrestha@gmail.com>
2024-06-07 22:29:52 +0545

    Added access tokens to return json

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -4,6 +4,7 @@ from google.oauth2 import id_token
 from google.auth.transport import requests
 from user.models import User
 from django.core.exceptions import ValidationError
+from rest_framework_simplejwt.tokens import RefreshToken,AccessToken
 
 class UserSerializer(BaseUserSerializer):
     class Meta(BaseUserSerializer.Meta):
@@ -18,6 +19,15 @@ class GoogleSignInSerializer(serializers.ModelSerializer):
         model=User
         fields=["id","nickname","given_name","family_name"]
 
+    def to_representation(self, user):
+        data = super().to_representation(user)
+        # Get access and refresh tokens
+        refresh = RefreshToken.for_user(user)
+        access = AccessToken.for_user(user)
+        # Include refresh token in the serialized data
+        data['refresh_token'] = str(refresh)
+        data['access_token']=str(access)
 
+        return data
 
     
--- backend/user/views.py
+++ backend/user/views.py
@@ -8,10 +8,11 @@ from user.models import User
 from .serializers import GoogleSignInSerializer
 # Create your views here.
 class GoogleSignInViewSet(ModelViewSet):
+    http_method_names = ['post','head','options']
     queryset = User.objects.all()
     def get_serializer_class(self):        
         if self.request.method == 'POST':
             return GoogleSignInSerializer
-        return GoogleSignInSerializer
+        
         
             
