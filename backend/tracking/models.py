737085e3066f9348de41b9941fc6b460bec55960
AnkitShrestha-Xenserv <aankitshrestha@gmail.com>
2024-06-08 00:31:51 +0545

    Added Leaderboard table

--- backend/tracking/models.py
+++ backend/tracking/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Exercises(models.Model):
@@ -6,3 +7,14 @@ class Exercises(models.Model):
     description = models.CharField(max_length=300)
     created_at = models.DateTimeField(auto_now_add=True)
 
+class Leaderboard(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    exercise_type = models.ForeignKey(Exercises,on_delete=models.CASCADE)
+    personal_count = models.IntegerField(default=0)
+    competitive_count = models.IntegerField(default=0)
+    personal_highest = models.IntegerField(default=0)
+    competitive_highest = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user','exercise_type')
