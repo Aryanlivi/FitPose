0a967cbbed96abc7f5c3d0bc2bb9df050d231257
PranavRJoshi <pranavrjoshi1@gmail.com>
2024-06-08 00:53:15 +0545

    update the app component
    add navbar, sidebar, and home component

--- frontend/find-x/src/App.js
+++ frontend/find-x/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './styles.css';
-import LoginButton from './components/LoginButton';
-import LogoutButton from './components/LogoutButton';
-import Profile from './Pages/Profile';
 import { useAuth0 } from '@auth0/auth0-react';
 import Home from './Pages/Home';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginButton from './components/LoginButton';
+
+const Dashboard = () => <h1>Dashboard</h1>;
+const Profile = () => <h1>Profile</h1>;
+const Settings = () => <h1>Settings</h1>;
 
 function App() {
 
@@ -13,10 +18,18 @@ function App() {
   console.log("Current User: ", user);
 
   return (
-    <main>
-      <Home />
-      <Profile />
-    </main>
+    <Router>
+    <div>
+        <Navbar />
+        <Sidebar />
+        <Routes>
+            <Route path="/dashboard" component={Dashboard} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/settings" component={Settings} />
+        </Routes>
+        <Home />
+    </div>
+</Router>
   );
 }
 
