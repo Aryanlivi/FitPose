501f1579384ca6038042b32538131f74d123898b
Pranav Ram Joshi <pranavramjoshi@Pranav-MacBook-Pro.local>
2024-06-07 18:35:52 +0545

    add profile component

--- frontend/find-x/src/App.js
+++ frontend/find-x/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import LoginButton from './components/LoginButton';
 import LogoutButton from './components/LogoutButton';
+import Profile from './components/Profile';
 import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <h1>Auth0 Login</h1>
       <LoginButton />
       <LogoutButton />
+      <Profile />
     </main>
   );
 }
---
+++ frontend/find-x/src/components/Profile.js
@@ -0,0 +1,17 @@
+import { useAuth0 } from '@auth0/auth0-react'
+import React from 'react'
+
+const Profile = () => {
+
+    const { user, isAuthenticated } = useAuth0();
+
+    return (
+        isAuthenticated && (
+            <article className='column'>
+                { user?.picture && <img src={user.picture} alt={user?.name} /> }
+            </article>
+        )
+    )
+}
+
+export default Profile;
