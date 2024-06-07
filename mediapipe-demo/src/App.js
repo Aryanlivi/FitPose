b0ff95cbcdebdaf34e42d8876263bdba4db7fa3d
Aryanlivi <aryanlivi@gmail.com>
2024-06-07 16:11:37 +0545

    added media pipe for testing

---
+++ mediapipe-demo/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import PoseDetection from './PoseDetection';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <PoseDetection />
+      </header>
+    </div>
+  );
+}
+
+export default App;
