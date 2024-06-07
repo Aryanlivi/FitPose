2faf3e730974d6d4a0eb673ce2610083a2399b5f
PranavRJoshi <pranavrjoshi1@gmail.com>
2024-06-08 00:50:54 +0545

    update home page
    add filler text and divide the home page into components

--- frontend/find-x/src/Pages/Home.js
+++ frontend/find-x/src/Pages/Home.js
@@ -4,16 +4,17 @@ import LoginButton from '../components/LoginButton';
 
 const Home = () => {
   return (
-    <>
-        <div className='flex border-solid border-2 border-sky-500 '>
-            <div className='flex justify-center items-center'>
-                <LandingScreen />
-            </div>
-            <div className='flex justify-right items-right'>
-                <LoginButton />
-            </div>
+    <React.Fragment>
+        <div className="ml-64 pt-16 p-4">
+            <h1 className="text-2xl font-bold mb-4">Main Content Area</h1>
+            <p>This is the main content area. Future components will be added here.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vel turpis feugiat, posuere eros in, congue nunc. Integer vel dolor nec tortor tincidunt convallis.</p>
+            <p>Praesent viverra, justo a aliquet dapibus, purus ligula facilisis leo, vitae auctor lacus nulla vel dolor. Donec vel dignissim tortor.</p>
+            <p>Phasellus scelerisque vehicula quam a auctor. Sed vel lectus vitae lorem ultricies fermentum a nec augue.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vel turpis feugiat, posuere eros in, congue nunc. Integer vel dolor nec tortor tincidunt convallis.</p>
+            <p>Praesent viverra, justo a aliquet dapibus, purus ligula facilisis leo, vitae auctor lacus nulla vel dolor. Donec vel dignissim tortor.</p>
         </div>
-    </>
+    </React.Fragment>
   )
 }
 
