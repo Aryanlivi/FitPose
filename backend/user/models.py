88d3bf394afd704578bccc43b310549d470b7d0e
AnkitShrestha-Xenserv <aankitshrestha@gmail.com>
2024-06-07 16:28:02 +0545

    Basic user model

---
+++ backend/user/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class User(AbstractUser):
+    first_name = models.CharField(max_length=150,blank=False)
+    last_name = models.CharField(max_length=150,blank=False)
+    nickname = models.CharField(max_length=150,blank=False)
+    
