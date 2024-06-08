5dacb01cbb779352588317f4bd95ca901e7fdc7b
PranavRJoshi <pranavrjoshi1@gmail.com>
2024-06-08 14:37:34 +0545

    update ui components
    update ui for home and sidebar

--- frontend/find-x/src/Pages/Home.js
+++ frontend/find-x/src/Pages/Home.js
@@ -7,20 +7,22 @@ import PoseDetection from '../context/PoseDetection';
 
 const Home = () => {
   return (
-    <div className="absolute top-16 right-0 bottom-0 flex">
-        <div style={{ display: 'fixed', flexDirection: 'column' }}>
-            <div style={{ height:'300px' ,width:'300px', margin: '10px',borderRadius: '10px', borderColor: 'black', borderWidth: '2px', padding:'16px'  }} className="w-[30%]">
-            <Leaderboard />
+    <div style={{ display: 'flex', flexDirection: 'row' , marginLeft:'180px', marginTop:'75px'}}>
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+            <div style={{ display: 'fixed', flexDirection: 'column' , marginTop:'-19px' }}>
+                <div style={{ height:'300px' ,width:'300px', margin: '20px',borderRadius: '10px', borderColor: 'black', borderWidth: '2px', padding:'16px'  }} className="w-[30%]">
+                    <Leaderboard />
+                </div>
+                <div style={{ height:'300px', width:'300px',margin:'20px', marginRight:'40px',borderRadius: '10px', borderColor: 'black', borderWidth: '2px', padding:'16px' }} className="w-[30%]">
+                    <Leaderboard />
+                </div>
             </div>
-            <div style={{ height:'300px', width:'300px',margin:'10px',borderRadius: '10px', borderColor: 'black', borderWidth: '2px', padding:'16px' }} className="w-[30%]">
-            <Leaderboard />
-            </div>
-            </div>
+        </div>
     
-      <div className="w-7/10 bg-gray-200 p-4">
-        {/* <PoseVideoDetection /> */}
-        <PoseDetection />
-      </div>
+        <div className="w-7/10 bg-gray-200 p-4">
+            {/* <PoseVideoDetection /> */}
+            <PoseDetection />
+        </div>
     </div>
   )
 }
