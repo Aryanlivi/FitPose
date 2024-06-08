800b38699aee0826fb8bc4c71175db1376178c87
Aryanlivi <aryanlivi@gmail.com>
2024-06-08 09:34:05 +0545

    diabled leaderboard signal

--- backend/tracking/apps.py
+++ backend/tracking/apps.py
@@ -5,5 +5,5 @@ class TrackingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tracking'
 
-    def ready(self) -> None:
-        import tracking.signals.handlers
+    # def ready(self) -> None:
+    #     import tracking.signals.handlers
--- backend/tracking/signals/handlers.py
+++ backend/tracking/signals/handlers.py
@@ -1,10 +1,10 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.conf import settings
-from tracking.models import Leaderboard
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
+# from django.conf import settings
+# from tracking.models import Leaderboard
 
-@receiver(post_save,sender = settings.AUTH_USER_MODEL)
-def create_leaderboard_for_new_user(sender,**kwargs):
-    user=kwargs['instance']
-    if kwargs['created'] and (user.is_superuser==False) and (user.is_staff==False):
-        leaderboard=Leaderboard.objects.create(user=user)
+# @receiver(post_save,sender = settings.AUTH_USER_MODEL)
+# def create_leaderboard_for_new_user(sender,**kwargs):
+#     user=kwargs['instance']
+#     if kwargs['created'] and (user.is_superuser==False) and (user.is_staff==False):
+#         leaderboard=Leaderboard.objects.create(user=user)
