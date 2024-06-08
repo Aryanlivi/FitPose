7654311e07c0c7ede65f6b141f2eaa43c29a7944
PranavRJoshi <pranavrjoshi1@gmail.com>
2024-06-08 13:39:25 +0545

    integrate mediapipe into ui

--- frontend/find-x/src/Pages/Home.js
+++ frontend/find-x/src/Pages/Home.js
@@ -2,6 +2,8 @@ import React from 'react';
 import LandingScreen from '../components/LandingScreen';
 import LoginButton from '../components/LoginButton';
 import Leaderboard from '../components/Leaderboard';
+import PoseVideoDetection from '../context/PoseVideoDetection';
+import PoseDetection from '../context/PoseDetection';
 
 const Home = () => {
   return (
@@ -16,25 +18,8 @@ const Home = () => {
             </div>
     
       <div className="w-7/10 bg-gray-200 p-4">
-        {/* Right Division */}
-        <h1 className="text-xl font-bold mb-4">Right Division</h1>
-        <p>This is the right division content.</p>
-        {/* Example Table */}
-        <h1>Lorem ipsum Lorem ipsum Lorem ipsum v Lorem ipsum Lorem ipsum v v Lorem ipsum Lorem ipsum Lorem ipsum v Lorem ipsum v v</h1>
-        <table className="w-full mt-4">
-          <thead>
-            <tr>
-              <th className="border px-4 py-2">Header 1</th>
-              <th className="border px-4 py-2">Header 2</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td className="border px-4 py-2">Data 1</td>
-              <td className="border px-4 py-2">Data 2</td>
-            </tr>
-          </tbody>
-        </table>
+        {/* <PoseVideoDetection /> */}
+        <PoseDetection />
       </div>
     </div>
   )
