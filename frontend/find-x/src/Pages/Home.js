49bd66eb9b3be0ac87bb36fc0d7fc3bdc6c34732
PranavRJoshi <pranavrjoshi1@gmail.com>
2024-06-08 11:50:30 +0545

    update home component

--- frontend/find-x/src/Pages/Home.js
+++ frontend/find-x/src/Pages/Home.js
@@ -5,10 +5,16 @@ import Leaderboard from '../components/Leaderboard';
 
 const Home = () => {
   return (
-    <div className="absolute top-16 right-0 bottom-0 bg-gray-100 flex">
-      <div className="w-3/10 bg-white">
-        <Leaderboard />
-      </div>
+    <div className="absolute top-16 right-0 bottom-0 flex">
+        <div style={{ display: 'fixed', flexDirection: 'column' }}>
+            <div style={{ height:'300px' ,width:'300px', margin: '10px',borderRadius: '10px', borderColor: 'black', borderWidth: '2px', padding:'16px'  }} className="w-[30%]">
+            <Leaderboard />
+            </div>
+            <div style={{ height:'300px', width:'300px',margin:'10px',borderRadius: '10px', borderColor: 'black', borderWidth: '2px', padding:'16px' }} className="w-[30%]">
+            <Leaderboard />
+            </div>
+            </div>
+    
       <div className="w-7/10 bg-gray-200 p-4">
         {/* Right Division */}
         <h1 className="text-xl font-bold mb-4">Right Division</h1>
